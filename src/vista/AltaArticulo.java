03b29e0861748afff51581ea09baa390837dd96a
PeyTheCrafter <peythecrafter@gmail.com>
2018-05-23 09:49:50 +0200

    Ui con ventanas dinamicas

---
+++ src/vista/AltaArticulo.java
@@ -0,0 +1,82 @@
+package vista;
+
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class AltaArticulo extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Create the panel.
+	 */
+	public AltaArticulo() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel = new JLabel("Alta Art\u00EDculo");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 3;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 2;
+		gbc_lblNewLabel.gridy = 1;
+		add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
+		gbc_lblNombre.anchor = GridBagConstraints.EAST;
+		gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNombre.gridx = 1;
+		gbc_lblNombre.gridy = 2;
+		add(lblNombre, gbc_lblNombre);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 3;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 2;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblPrecio = new JLabel("Precio");
+		GridBagConstraints gbc_lblPrecio = new GridBagConstraints();
+		gbc_lblPrecio.anchor = GridBagConstraints.EAST;
+		gbc_lblPrecio.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPrecio.gridx = 1;
+		gbc_lblPrecio.gridy = 3;
+		add(lblPrecio, gbc_lblPrecio);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 3;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		GridBagConstraints gbc_btnAceptar = new GridBagConstraints();
+		gbc_btnAceptar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAceptar.gridx = 4;
+		gbc_btnAceptar.gridy = 4;
+		add(btnAceptar, gbc_btnAceptar);
+
+	}
+
+}
--- src/vista/UI.java
+++ src/vista/UI.java
@@ -1,45 +1,102 @@
 package vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JInternalFrame;
 import java.awt.Cursor;
 import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 import javax.swing.border.LineBorder;
 import java.awt.Color;
-import javax.swing.JLayeredPane;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.awt.Component;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class UI extends JFrame {
 
 	private JPanel contentPane;
+	private BufferedImage img;
 
 	/**
 	 * Create the frame.
 	 */
 	public UI() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 553, 426);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnArtculo = new JMenu("Art\u00EDculo");
+		menuBar.add(mnArtculo);
+
+		JMenuItem mntmAltaArticulo = new JMenuItem("Alta");
+		mntmAltaArticulo.setActionCommand("AltaArticulo");
+		mntmAltaArticulo.setName("AltaArticulo");
+		mnArtculo.add(mntmAltaArticulo);
+
+		JMenuItem mntmConsultaArticulo = new JMenuItem("Consulta");
+		mnArtculo.add(mntmConsultaArticulo);
+
+		JMenuItem mntmModificacinArticulo = new JMenuItem("Modificaci\u00F3n");
+		mnArtculo.add(mntmModificacinArticulo);
+
+		JMenu mnCliente = new JMenu("Cliente");
+		menuBar.add(mnCliente);
+
+		JMenuItem mntmAltaCliente = new JMenuItem("Alta");
+		mnCliente.add(mntmAltaCliente);
+
+		JMenuItem mntmBajaCliente = new JMenuItem("Baja");
+		mnCliente.add(mntmBajaCliente);
+
+		JMenu mnPedido = new JMenu("Pedido");
+		menuBar.add(mnPedido);
+
+		JMenuItem mntmAltaPedido = new JMenuItem("Alta");
+		mnPedido.add(mntmAltaPedido);
 		contentPane = new JPanel();
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
+
+		mntmAltaArticulo.addActionListener(e -> crearVentana(e.getActionCommand()));
 	}
 
+	private void crearVentana(String clase) {
+		JInternalFrame frame = new JInternalFrame("Alta artículo", false, true, false);
+		Class<?> cls = null;
+		try {
+			cls = Class.forName("vista." + clase);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Object pane = null;
+		try {
+			pane = cls.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		frame.getContentPane().add((Component) pane);
+		frame.setVisible(true);
+		frame.pack();
+		contentPane.add(frame);
+	}
 }
