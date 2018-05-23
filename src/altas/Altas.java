72621e9b273adbc679663cb24ea43afa10f89a04
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-05-23 13:46:38 +0200

    creacion de acciones

---
+++ src/altas/Altas.java
@@ -0,0 +1,32 @@
+package altas;
+
+import control.ParaUI;
+
+public class Altas {
+	
+	private ParaUI paraui;
+
+	public Altas(ParaUI paraui) {
+		super();
+		this.paraui = paraui;
+	}
+	
+	
+	/**
+	 * hay que crear la sobrecarga de todo
+	 */
+	
+	public void crearArticulo() {
+		
+	}
+	
+	public void crearCliente() {
+		
+	}
+	
+	public void crearPedido() {
+		
+	}
+	
+
+}
