97849ab99ef7d74ba87fce5c270446a9af6e018b
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-05-28 11:03:17 +0200

    gestores altas rellenos

---
+++ src/acciones/GestorAltaPedido.java
@@ -0,0 +1,18 @@
+package acciones;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GestorAltaPedido implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+//		if(paraui.altas.crearPedido(meter todos los parametros y hay que cogerlos del paraui/vista)){
+//		paraui.insertarMensaje("pedido creado");
+//	}else {
+//		paraui.insertarMensaje("ERROR");
+//	}
+
+	}
+
+}
