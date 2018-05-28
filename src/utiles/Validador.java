e2a928c2f77f07f25bdf19b59cbf2c1eea0f2ee2
Jose <HellsockPS3@gmail.com>
2018-05-28 10:06:39 +0200

    Validador

--- src/utiles/Validador.java
+++ src/utiles/Validador.java
@@ -1,5 +1,30 @@
 package utiles;
 
-public class Validador {
+import java.util.regex.Pattern;
 
+public class Validador {
+//Devuelve true si esta vacio y false si no.
+public boolean comprobarVacio(String campo) {
+	if (campo.isEmpty()) {
+		return true;
+	}
+	return false;
+}
+
+public boolean comprobarTelefono(String telefono) {
+	String telefonoRegexp = "[0-9]{9}$";
+	return comprobacionRegular(telefonoRegexp, telefono);
+}
+
+public boolean comprobarNif(String dni) {
+	String dniRegexp = "\\d{8}[A-HJ-NP-TV-Z]";
+	return comprobacionRegular(dniRegexp, dni);
+}
+
+private boolean comprobacionRegular(String expresionRegular, String input) {
+	if (Pattern.matches(expresionRegular, input)) {
+		return true;
+	}
+	return false;
+}
 }
