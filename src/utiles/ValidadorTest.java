2f8caa67f9f2c1de101f6049ca537800acb07322
Jose <HellsockPS3@gmail.com>
2018-05-28 12:08:40 +0200

    ValidadorTest

---
+++ src/utiles/ValidadorTest.java
@@ -0,0 +1,44 @@
+package utiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+
+class ValidadorTest {
+Validador instancia = new Validador();
+	
+	@Test
+	void testComprobarVacio() {
+		String[] vacio = {"","H"};
+		assertTrue(instancia.comprobarVacio(vacio[0]));
+		assertFalse(instancia.comprobarVacio(vacio[1]));
+	}
+
+	@Test
+	void testComprobarTelefono() {
+		String[] telefonos = {"696849236","8387923","69684H236","696 49236"};
+		for (int i = 0; i < telefonos.length; i++) {
+			if (i==0) {
+				assertTrue(instancia.comprobarTelefono(telefonos[i]));
+			}else {
+				assertFalse(instancia.comprobarTelefono(telefonos[i]));
+			}
+		}
+	}
+
+	@Test
+	void testComprobarNif() {
+		String[] nifs = {"45969672D","459672D","4596967HD","4596/672D"};
+		for (int i = 0; i < nifs.length; i++) {
+			if (i==0) {
+				assertTrue(instancia.comprobarNif(nifs[i]));
+			}else {
+				assertFalse(instancia.comprobarNif(nifs[i]));
+			}
+		}
+	}
+
+}
