8b955368ac709a22964106b96f43cb25a84d3252
PeyTheCrafter <peythecrafter@gmail.com>
2018-05-29 11:51:44 +0200

    Arreglo asserts

--- src/acciones/botones/listenerBtnAltaCliente.java
+++ src/acciones/botones/listenerBtnAltaCliente.java
@@ -17,22 +17,17 @@ public class listenerBtnAltaCliente implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		AltaCliente ventana = this.paraUI.getPanelAltaCliente();
-		assert !(this.paraUI.getValidador().comprobarVacio(ventana.getTxtRazon().getText()) == true) : "razon vacia";
-		assert !(this.paraUI.getValidador().comprobarVacio(ventana.getTxtNif().getText()) == true) : "nif vacia";
-		assert !(this.paraUI.getValidador()
-				.comprobarVacio(ventana.getTxtDireccion().getText()) == true) : "direccion vacia";
-		assert !(this.paraUI.getValidador()
-				.comprobarVacio(ventana.getTxtTelefono().getText()) == true) : "telefono vacia";
-		assert !(this.paraUI.getValidador()
-				.comprobarTelefono(ventana.getTxtTelefono().getText()) == true) : "telefono erroneo";
-		assert !(this.paraUI.getValidador().comprobarNif(ventana.getTxtNif().getText()) == true) : "nif erroneo";
+		assert !this.paraUI.getValidador().comprobarVacio(ventana.getTxtRazon().getText()) : "razon vacia";
+		assert !this.paraUI.getValidador().comprobarVacio(ventana.getTxtNif().getText()) : "nif vacia";
+		assert !this.paraUI.getValidador().comprobarVacio(ventana.getTxtDireccion().getText()) : "direccion vacia";
+		assert !this.paraUI.getValidador().comprobarVacio(ventana.getTxtTelefono().getText()) : "telefono vacia";
+		assert !this.paraUI.getValidador().comprobarTelefono(ventana.getTxtTelefono().getText()) : "telefono erroneo";
+		assert !this.paraUI.getValidador().comprobarNif(ventana.getTxtNif().getText()) : "nif erroneo";
 		if (this.paraUI.getAltas().crearCliente(ventana.getTxtRazon().getText(), ventana.getTxtNif().getText(),
 				ventana.getTxtDireccion().getText(), ventana.getTxtTelefono().getText())) {
 			ventana.getTxtMensajeAltaCliente().setText("cliente creado");
 		} else {
 			ventana.getTxtMensajeAltaCliente().setText("ERROR");
 		}
-
 	}
-
 }
