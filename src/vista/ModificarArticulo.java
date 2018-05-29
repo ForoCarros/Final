a8b9781919fab6e05b43a780573973fdc372582f
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-05-29 19:03:59 +0200

    añadido la modificacion

--- src/vista/ModificarArticulo.java
+++ src/vista/ModificarArticulo.java
@@ -26,13 +26,14 @@ public class ModificarArticulo extends JPanel {
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblModificarArticulo = new JLabel("MODIFICAR ARTICULO");
 		lblModificarArticulo.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		lblModificarArticulo.setFont(new Font("Snap ITC", Font.BOLD, 30));
 		GridBagConstraints gbc_lblModificarArticulo = new GridBagConstraints();
+		gbc_lblModificarArticulo.fill = GridBagConstraints.VERTICAL;
 		gbc_lblModificarArticulo.gridwidth = 2;
 		gbc_lblModificarArticulo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblModificarArticulo.gridx = 1;
@@ -41,7 +42,6 @@ public class ModificarArticulo extends JPanel {
 		
 		JLabel lblSeleccionaArticulo = new JLabel("Selecciona Articulo");
 		GridBagConstraints gbc_lblSeleccionaArticulo = new GridBagConstraints();
-		gbc_lblSeleccionaArticulo.anchor = GridBagConstraints.EAST;
 		gbc_lblSeleccionaArticulo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblSeleccionaArticulo.gridx = 1;
 		gbc_lblSeleccionaArticulo.gridy = 3;
@@ -57,14 +57,12 @@ public class ModificarArticulo extends JPanel {
 		
 		JLabel lblIntroduceNuevoPrecio = new JLabel("Introduce Nuevo Precio:");
 		GridBagConstraints gbc_lblIntroduceNuevoPrecio = new GridBagConstraints();
-		gbc_lblIntroduceNuevoPrecio.anchor = GridBagConstraints.EAST;
 		gbc_lblIntroduceNuevoPrecio.insets = new Insets(0, 0, 5, 5);
 		gbc_lblIntroduceNuevoPrecio.gridx = 1;
 		gbc_lblIntroduceNuevoPrecio.gridy = 4;
 		add(lblIntroduceNuevoPrecio, gbc_lblIntroduceNuevoPrecio);
 		
 		txtNuevoPrecio = new JTextField();
-		txtNuevoPrecio.setEditable(false);
 		GridBagConstraints gbc_txtNuevoPrecio = new GridBagConstraints();
 		gbc_txtNuevoPrecio.insets = new Insets(0, 0, 5, 5);
 		gbc_txtNuevoPrecio.fill = GridBagConstraints.HORIZONTAL;
@@ -75,6 +73,7 @@ public class ModificarArticulo extends JPanel {
 		
 		btnActualizarPrecio = new JButton("Actualizar");
 		GridBagConstraints gbc_btnActualizarPrecio = new GridBagConstraints();
+		gbc_btnActualizarPrecio.fill = GridBagConstraints.VERTICAL;
 		gbc_btnActualizarPrecio.insets = new Insets(0, 0, 5, 5);
 		gbc_btnActualizarPrecio.gridx = 2;
 		gbc_btnActualizarPrecio.gridy = 6;
@@ -82,7 +81,6 @@ public class ModificarArticulo extends JPanel {
 		
 		JLabel lblMensaje = new JLabel("Mensaje:");
 		GridBagConstraints gbc_lblMensaje = new GridBagConstraints();
-		gbc_lblMensaje.anchor = GridBagConstraints.EAST;
 		gbc_lblMensaje.insets = new Insets(0, 0, 5, 5);
 		gbc_lblMensaje.gridx = 1;
 		gbc_lblMensaje.gridy = 7;
@@ -99,5 +97,23 @@ public class ModificarArticulo extends JPanel {
 		txtMensajeModificar.setColumns(10);
 
 	}
+
+	public JTextField getTxtNuevoPrecio() {
+		return txtNuevoPrecio;
+	}
+
+	public JButton getBtnActualizarPrecio() {
+		return btnActualizarPrecio;
+	}
+
+	public JTextField getTxtMensajeModificar() {
+		return txtMensajeModificar;
+	}
+
+	public JComboBox getComboBox() {
+		return comboBox;
+	}
+	
+	
 
 }
