f6a5c5807336f57b2474f1e9771e7ae8afd58cd9
PeyTheCrafter <peythecrafter@gmail.com>
2018-05-30 09:57:50 +0200

    Almacen para clientes (indizado)

---
+++ src/modelo/acceso/AlmacenCliente.java
@@ -0,0 +1,89 @@
+package modelo.acceso;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class AlmacenCliente<T, K> {
+	private String pathIndice;
+	private String pathDatos;
+	private TreeMap<K, Integer> indice;
+	DAO<T> dao;
+
+	public AlmacenCliente(String ruta) {
+		super();
+		this.pathIndice = ruta + "/clientes.index";
+		this.pathDatos = ruta + "/clientes.data";
+		comprobarExistente(ruta);
+		assert validate();
+		this.indice = new TreeMap<>();
+		dao = new DAO<>();
+	}
+
+	private void comprobarExistente(String path) {
+		File ruta = new File(path);
+		File datos = new File(this.pathDatos);
+		File indices = new File(this.pathIndice);
+		if(!ruta.exists()) {
+			ruta.mkdirs();
+		}
+		if (!datos.exists()) {
+			try {
+				datos.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (!indices.exists()) {
+			try {
+				indices.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private boolean validate() {
+		return this.pathIndice != null && this.pathDatos != null;
+	}
+
+	public T obtener(K k) {
+		assert k != null;
+		indice = (TreeMap<K, Integer>) dao.leer(pathIndice);
+		if (indice == null) {
+			indice = new TreeMap<>();
+			new DAO<>().grabar(pathIndice, indice);
+		}
+		T retorno = null;
+		Integer posicion = indice.get(k);
+		if (posicion != null) {
+			retorno = (T) dao.leer(pathDatos, posicion);
+		}
+		return retorno;
+	}
+
+	public boolean grabar(T t, K k) {
+		assert k != null && t != null;
+		boolean retorno = false;
+		// miro el ultimo indice. siempre hay un mapa aqui
+		Entry<K, Integer> lastEntry = indice.lastEntry();
+		Integer value = 0;
+		// si es el primer elemento lastentry sera null
+		if (lastEntry != null) {
+			value = lastEntry.getValue() + 1;
+		}
+		if (indice.put(k, value) == null) {
+			if (dao.grabar(pathDatos, t, true)) {
+				retorno = true;
+				new DAO<>().grabar(pathIndice, indice);
+			} else {
+				// Si no se graba bien actualizamos el indice con la version grabada
+				indice = (TreeMap<K, Integer>) dao.leer(pathIndice);
+			}
+		}
+		return retorno;
+	}
+}
