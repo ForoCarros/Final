699de34f23adadeacca1724da01d141a64ff1332
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-06-02 11:21:48 +0200

    almacen arreglado

--- src/modelo/acceso/AlmacenCliente.java
+++ src/modelo/acceso/AlmacenCliente.java
@@ -9,7 +9,7 @@ public class AlmacenCliente<T, K> {
 	private String pathIndice;
 	private String pathDatos;
 	private TreeMap<K, Integer> indice;
-	DAO<T> dao;
+	private DAO<T> dao;
 
 	public AlmacenCliente(String ruta) {
 		super();
@@ -52,15 +52,11 @@ public class AlmacenCliente<T, K> {
 
 	public T obtener(K k) {
 		assert k != null;
-		indice = (TreeMap<K, Integer>) dao.leer(pathIndice);
-		if (indice == null) {
-			indice = new TreeMap<>();
-			new DAO<>().grabar(pathIndice, indice);
-		}
 		T retorno = null;
+		indice = (TreeMap<K, Integer>) new DAO().leer(pathIndice);
 		Integer posicion = indice.get(k);
 		if (posicion != null) {
-			retorno = (T) dao.leer(pathDatos, posicion);
+			retorno = (T) new DAO().leer(pathDatos, posicion);
 		}
 		return retorno;
 	}
@@ -68,20 +64,17 @@ public class AlmacenCliente<T, K> {
 	public boolean grabar(T t, K k) {
 		assert k != null && t != null;
 		boolean retorno = false;
-		// miro el ultimo indice. siempre hay un mapa aqui
 		Entry<K, Integer> lastEntry = indice.lastEntry();
 		Integer value = 0;
-		// si es el primer elemento lastentry sera null
 		if (lastEntry != null) {
 			value = lastEntry.getValue() + 1;
 		}
 		if (indice.put(k, value) == null) {
-			if (dao.grabar(pathDatos, t, true)) {
+			if (new DAO<>().grabar(pathDatos, t, true)) {
 				retorno = true;
 				new DAO<>().grabar(pathIndice, indice);
 			} else {
-				// Si no se graba bien actualizamos el indice con la version grabada
-				indice = (TreeMap<K, Integer>) dao.leer(pathIndice);
+				new DAO<>().grabar(pathIndice, indice);
 			}
 		}
 		return retorno;
@@ -93,11 +86,7 @@ public class AlmacenCliente<T, K> {
 	 * @return
 	 */
 	public Object getIndice() {
-		if (comprobarExiste(pathIndice)) {
-			return (TreeMap) new DAO<T>().leer(pathIndice);
-		} else {
-			return null;
-		}
+		return (TreeMap) new DAO<>().leer(pathIndice);
 	}
 
 	/**
