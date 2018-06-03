213c5561da8f4d7b81b025748c48a6b0b7d75b0e
Jose <HellsockPS3@gmail.com>
2018-06-03 20:57:06 +0200

    Holi :3

---
+++ src/logica/Busquedas.java
@@ -0,0 +1,36 @@
+package logica;
+
+import control.ParaUI;
+import modelo.acceso.AlmacenArticulo;
+import modelo.acceso.AlmacenCliente;
+
+public class Busquedas {
+	
+	private ParaUI paraui;
+	private Logica logica;
+	
+	public Busquedas(ParaUI paraui) {
+		super();
+		this.paraui = paraui;
+		this.logica = new Logica();
+	}
+	
+	
+	/**
+	 * hay que crear la sobrecarga de todo
+	 */
+	
+	
+	public void buscarArticulo() {
+		
+	}
+	
+	public void buscarCliente() {
+		
+	}
+	
+	public void buscarPedido() {
+		
+	}
+
+}
--- src/logica/Logica.java
+++ src/logica/Logica.java
@@ -29,65 +29,114 @@ public class Logica {
 
 	public Logica() {
 		this.dao = new DAO<>();
-		this.almacenCliente = new AlmacenCliente("data/clientes");
 		this.almacenArticulo = new AlmacenArticulo("data/articulos");
+		this.almacenCliente = new AlmacenCliente("data/clientes");
 	}
 
-	/**
-	 * graba en el fichero el cliente creado
-	 * 
-	 * @param cliente
-	 * @return
-	 */
 	public boolean crear(Cliente cliente) {
 		return almacenCliente.grabar(cliente, cliente.getDniCif());
 	}
 
-	/**
-	 * graba en el fichero el articulo creado
-	 * 
-	 * @param articulo
-	 * @return
-	 */
 	public boolean crear(Articulo articulo) {
 		System.out.println("estoy en logica, voy almacen");
 		return almacenArticulo.grabar(articulo, articulo.getIdArticulo(), articulo.getNombre());
 	}
 
-	/**
-	 * graba en un fichero el pedido creado
-	 * 
-	 * @param pedido
-	 * @return
-	 */
 	public boolean crear(Pedido pedido) {
 		return true;
 	}
-
+	
+	public Articulo buscarArticulo(String nombreArticulo) {
+		return (Articulo) almacenArticulo.leer(nombreArticulo);
+	}
+	
+	public Cliente buscarCliente(String razonSocial) {
+		return (Cliente) almacenCliente.obtener(razonSocial);
+	}
+	
+	
 	/**
 	 * obtiene el ultimo numero del indice en los articulos
-	 * 
 	 * @return el ultimo numero
 	 */
 	public int dameUltimoNumero() {
 		int numIndice = 0;
 		TreeMap indice = (TreeMap) new AlmacenArticulo<>("./data/articulos").getIndice();
 		Set clave = indice.keySet();
-		numIndice = clave.size();
+		numIndice=(clave.size())+1;
 		return numIndice;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/**
+	 * Graba en el archivo que le pasemos el objeto
+	 * 
+	 * @param object
+	 * @param ruta
+	 * @return true o false
+	 */
+	public boolean grabar(Object object, String ruta) {
+		return new DAO<Object>().grabar(ruta, object, true);
 	}
 
 	/**
-	 * elimina el cliente seleccionado en el comboBox
+	 * Introduce una ruta por parametro y se la da al dao
 	 * 
-	 * @param selectedItem
+	 * @param ruta
+	 * @return
+	 */
+	public Object dameRuta(String ruta) {
+		dao = new DAO<Object>();
+		return dao.leer(ruta);
+	}
+
+	/**
+	 * Devuelve true o false segun si encuentra en el archivo el objeto que buscamos
+	 * 
 	 * @return true o false
 	 */
-	public boolean eliminarCliente(Object selectedItem) {
-
-		// esto es un royo...buscas cliente en datos? y lo eliminas? y eliminas su
-		// referencia en el map? y en el archivo indice?
-
-		return false;
+	public Object obtener(Object object, String ruta) {
+		dao = new DAO<Object>();
+		Object dummy = null;
+		boolean iguales = false;
+		int contador = 0;
+		do {
+			dao.leer(ruta, contador);
+			iguales = comprobarDuplicado(dummy, object);
+			contador++;
+		} while (iguales = false && dummy != null);
+		return iguales;
 	}
+
+	/**
+	 * Comprueba entre dos objetos si son el mismo
+	 * 
+	 * @param object
+	 * @param object2
+	 * @return true o false
+	 */
+	public boolean comprobarDuplicado(Object object, Object object2) {
+		return object.equals(object2);
+	}
+
+	public void comprobarArchivoExistente(String ruta) {
+		File f = new File(ruta);
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+
 }
