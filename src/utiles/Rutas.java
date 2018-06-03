7b052ebf3b1cbfaab224e9d9b7602f8c610f7218
pablo <peythecrafter@gmail.com>
2018-06-03 13:07:58 +0200

    Fix

--- src/utiles/Rutas.java
+++ src/utiles/Rutas.java
@@ -1,18 +1,18 @@
 package utiles;
 
-public class Rutas {
+public final class Rutas {
 	// - Pedidos -
 	public final String pathNUP = "numeroUltimoPedido.data";
 	public final String pathPedidos = "./data/pedidos";
-	
+
 	// - Clientes -
-	
+
 	// - Artículos -
-	
+
 	// - PedidosTest -
-	
+
 	// - ClientesTest -
-	
+
 	// - ArtículosTest -
 	public final String pathNUPTest = "./Test/pedidos";
 	public final String pathPedidosTest = "./Test/pedidos";
