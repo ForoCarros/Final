dba37b74ff97d7588a4d5ea0df4689d244f939cf
pablo <peythecrafter@gmail.com>
2018-06-03 18:01:15 +0200

    Cambios

--- src/acciones/botones/listenerBtnAltaPedido.java
+++ src/acciones/botones/listenerBtnAltaPedido.java
@@ -3,6 +3,8 @@ package acciones.botones;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+
 import control.ParaUI;
 import vista.AltaPedido;
 
@@ -17,12 +19,9 @@ public class listenerBtnAltaPedido implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		AltaPedido ventana = this.paraUI.getPanelAltaPedido();
-		
-		//comprobaciones de todo
-		
 		System.out.println("pedido creado");
 		ventana.getTxtMensajePedido().setText("pedido creado");
-		
+		//this.paraUI.
 	}
 
 }
