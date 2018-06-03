f10da8a12e0acbbd2e8f39a5c318ed8a5f35b2a6
pablo <peythecrafter@gmail.com>
2018-06-03 12:57:13 +0200

    Clase para almacenar todas las rutas

---
+++ src/utiles/Rutas.java
@@ -0,0 +1,19 @@
+package utiles;
+
+public class Rutas {
+	// - Pedidos -
+	public final String pathNUP = "numeroUltimoPedido.data";
+	public final String pathPedidos = "./data/pedidos";
+	
+	// - Clientes -
+	
+	// - Artículos -
+	
+	// - PedidosTest -
+	
+	// - ClientesTest -
+	
+	// - ArtículosTest -
+	public final String pathNUPTest = "./Test/pedidos";
+	public final String pathPedidosTest = "./Test/pedidos";
+}
