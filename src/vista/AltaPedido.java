94a7f0171d7b5cfc90e5a002d47d4c2ae1b73d9b
pablo <peythecrafter@gmail.com>
2018-06-03 23:50:41 +0200

    un import

--- src/vista/AltaPedido.java
+++ src/vista/AltaPedido.java
@@ -6,6 +6,8 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JButton;
 import java.awt.Insets;
+import java.util.Vector;
+
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
@@ -13,6 +15,7 @@ import java.awt.Font;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class AltaPedido extends JPanel {
 	protected JTextField txtNumPedido;
@@ -24,18 +27,20 @@ public class AltaPedido extends JPanel {
 	protected JButton btnAnadirLinea;
 	protected JButton btnCrearPedido;
 	protected JTable lineasPedido;
+	private JScrollPane scrollPane;
 
 	/**
 	 * Create the panel.
 	 */
 	public AltaPedido() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblCrearPedido = new JLabel("CREAR PEDIDO");
 		lblCrearPedido.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCrearPedido.setBorder(new LineBorder(new Color(0, 0, 0), 3));
@@ -47,7 +52,7 @@ public class AltaPedido extends JPanel {
 		gbc_lblCrearPedido.gridx = 1;
 		gbc_lblCrearPedido.gridy = 1;
 		add(lblCrearPedido, gbc_lblCrearPedido);
-		
+
 		JLabel lblNPedido = new JLabel("N\u00BA Pedido");
 		lblNPedido.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNPedido = new GridBagConstraints();
@@ -56,7 +61,7 @@ public class AltaPedido extends JPanel {
 		gbc_lblNPedido.gridx = 1;
 		gbc_lblNPedido.gridy = 3;
 		add(lblNPedido, gbc_lblNPedido);
-		
+
 		txtNumPedido = new JTextField();
 		txtNumPedido.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_txtNumPedido = new GridBagConstraints();
@@ -67,14 +72,14 @@ public class AltaPedido extends JPanel {
 		gbc_txtNumPedido.gridy = 3;
 		add(txtNumPedido, gbc_txtNumPedido);
 		txtNumPedido.setColumns(10);
-		
+
 		JLabel lblCliente = new JLabel("Elige Cliente:");
 		GridBagConstraints gbc_lblCliente = new GridBagConstraints();
 		gbc_lblCliente.insets = new Insets(0, 0, 5, 5);
 		gbc_lblCliente.gridx = 1;
 		gbc_lblCliente.gridy = 4;
 		add(lblCliente, gbc_lblCliente);
-		
+
 		comboClientes = new JComboBox();
 		GridBagConstraints gbc_comboPedido = new GridBagConstraints();
 		gbc_comboPedido.gridwidth = 2;
@@ -83,7 +88,7 @@ public class AltaPedido extends JPanel {
 		gbc_comboPedido.gridx = 2;
 		gbc_comboPedido.gridy = 4;
 		add(comboClientes, gbc_comboPedido);
-		
+
 		comboArticulos = new JComboBox();
 		GridBagConstraints gbc_comboArticulos = new GridBagConstraints();
 		gbc_comboArticulos.insets = new Insets(0, 0, 5, 5);
@@ -91,7 +96,7 @@ public class AltaPedido extends JPanel {
 		gbc_comboArticulos.gridx = 1;
 		gbc_comboArticulos.gridy = 5;
 		add(comboArticulos, gbc_comboArticulos);
-		
+
 		JLabel lblCantidad = new JLabel("Inserta Cantidad:");
 		GridBagConstraints gbc_lblCantidad = new GridBagConstraints();
 		gbc_lblCantidad.anchor = GridBagConstraints.EAST;
@@ -99,7 +104,7 @@ public class AltaPedido extends JPanel {
 		gbc_lblCantidad.gridx = 2;
 		gbc_lblCantidad.gridy = 5;
 		add(lblCantidad, gbc_lblCantidad);
-		
+
 		txtCantidad = new JTextField();
 		GridBagConstraints gbc_txtCantidad = new GridBagConstraints();
 		gbc_txtCantidad.insets = new Insets(0, 0, 5, 5);
@@ -108,7 +113,7 @@ public class AltaPedido extends JPanel {
 		gbc_txtCantidad.gridy = 5;
 		add(txtCantidad, gbc_txtCantidad);
 		txtCantidad.setColumns(10);
-		
+
 		btnAnadirLinea = new JButton("A\u00F1adir");
 		GridBagConstraints gbc_btnAnadirArticulo = new GridBagConstraints();
 		gbc_btnAnadirArticulo.fill = GridBagConstraints.HORIZONTAL;
@@ -117,16 +122,25 @@ public class AltaPedido extends JPanel {
 		gbc_btnAnadirArticulo.gridx = 1;
 		gbc_btnAnadirArticulo.gridy = 6;
 		add(btnAnadirLinea, gbc_btnAnadirArticulo);
-		
-		lineasPedido = new JTable();
-		GridBagConstraints gbc_lineasPedido = new GridBagConstraints();
-		gbc_lineasPedido.gridwidth = 3;
-		gbc_lineasPedido.insets = new Insets(0, 0, 5, 5);
-		gbc_lineasPedido.fill = GridBagConstraints.BOTH;
-		gbc_lineasPedido.gridx = 1;
-		gbc_lineasPedido.gridy = 7;
-		add(lineasPedido, gbc_lineasPedido);
-		
+		Vector<Vector> rowData = new Vector<>();
+		Vector<String> columnNames = new Vector<>();
+		columnNames.add("Artículo");
+		columnNames.add("Cantidad");
+		columnNames.add("Precio unidad");
+		columnNames.add("Total");
+
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 3;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 7;
+		add(scrollPane, gbc_scrollPane);
+
+		lineasPedido = new JTable(rowData, columnNames);
+		scrollPane.setViewportView(lineasPedido);
+
 		JLabel lblPrecioTotal = new JLabel("Precio Total:");
 		GridBagConstraints gbc_lblPrecioTotal = new GridBagConstraints();
 		gbc_lblPrecioTotal.anchor = GridBagConstraints.EAST;
@@ -134,7 +148,7 @@ public class AltaPedido extends JPanel {
 		gbc_lblPrecioTotal.gridx = 2;
 		gbc_lblPrecioTotal.gridy = 8;
 		add(lblPrecioTotal, gbc_lblPrecioTotal);
-		
+
 		txtPrecioFinal = new JTextField();
 		txtPrecioFinal.setEditable(false);
 		txtPrecioFinal.setHorizontalAlignment(SwingConstants.CENTER);
@@ -145,7 +159,7 @@ public class AltaPedido extends JPanel {
 		gbc_txtPrecioFinal.gridy = 8;
 		add(txtPrecioFinal, gbc_txtPrecioFinal);
 		txtPrecioFinal.setColumns(10);
-		
+
 		JLabel lblMensaje = new JLabel("Mensaje:");
 		GridBagConstraints gbc_lblMensaje = new GridBagConstraints();
 		gbc_lblMensaje.anchor = GridBagConstraints.EAST;
@@ -153,7 +167,7 @@ public class AltaPedido extends JPanel {
 		gbc_lblMensaje.gridx = 1;
 		gbc_lblMensaje.gridy = 9;
 		add(lblMensaje, gbc_lblMensaje);
-		
+
 		txtMensajePedido = new JTextField();
 		txtMensajePedido.setEditable(false);
 		txtMensajePedido.setHorizontalAlignment(SwingConstants.CENTER);
@@ -165,7 +179,7 @@ public class AltaPedido extends JPanel {
 		gbc_txtMensajePedido.gridy = 9;
 		add(txtMensajePedido, gbc_txtMensajePedido);
 		txtMensajePedido.setColumns(10);
-		
+
 		btnCrearPedido = new JButton("Crear Pedido");
 		GridBagConstraints gbc_btnCrearPedido = new GridBagConstraints();
 		gbc_btnCrearPedido.insets = new Insets(0, 0, 5, 5);
@@ -209,5 +223,9 @@ public class AltaPedido extends JPanel {
 
 	public JButton getBtnCrearPedido() {
 		return btnCrearPedido;
+	}
+
+	public JTable getLineasPedido() {
+		return lineasPedido;
 	}
 }
