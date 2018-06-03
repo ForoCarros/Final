c0b8617e881b35fbd02540ca2cd0cd6001a2816d
pablo <peythecrafter@gmail.com>
2018-06-03 13:08:25 +0200

    AlmacenPedidoTst en progreso

---
+++ src/modelo/acceso/AlmacenPedidoTest.java
@@ -0,0 +1,36 @@
+package modelo.acceso;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class AlmacenPedidoTest {
+	AlmacenPedido almacen;
+	
+	@Before
+	void setUp() {
+		this.almacen = new AlmacenPedido();
+	}
+
+	@Test
+	void testAlmacenPedido() {
+		
+	}
+
+	@Test
+	void testComprobarExistente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGrabarNumero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLeerNumero() {
+		fail("Not yet implemented");
+	}
+
+}
